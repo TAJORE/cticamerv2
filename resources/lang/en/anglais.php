68e2ba32e23bf71fdb07a5f9b76e5c45a46fafce
DimitriKENGNE <m.dimitri.kengne@gmail.com>
2018-04-25 15:36:16 +0100

    Je refais les fichiers

---
+++ resources/lang/en/anglais.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Traduction pour la page anglais en anglais
+    |--------------------------------------------------------------------------
+    |
+    | plus d'info...
+    |
+    */
+
+
+    'descriptionANGL'=> 'English courses for all levels, accessible to all. Intensive Courses, Normal Training, Holyday Classes,
+                 Private Lessons, Corporate Training, for you give the perfect mastery of English',
+'keywordANGL'=> 'bilingualism, english, courses, become bilingual, language center, training, English courses, learning English, TOEFL, TOEIC, IELTS, Douala,
+             Bonamoussadi, Cameroon A1, A2, B1, B2, C1, C2',
+'title'=> 'English courses at Douala with Churchill Training Institute - A reference language center.',
+
+
+#Les formats disponibles
+
+'PlsFormats'=> 'Several options available:',
+'CoursIntens'=> 'Accelerated Training',
+'FormatReg'=> 'REGULAR TRAINING (REPL)',
+'FormatRegACC'=> 'ACCELERETED REGULAR TRAINING (REPL2)',
+'prEntrep'=> 'Corporate Training',
+'CoursVACCANCES'=> 'Holydays Courses',
+'CoursPARTICULIERS'=> 'Private Lessons',
+'diffNIVEAUX'=> 'Consult the different language levels',
+
+
+# Block Cours Disponibles
+
+'coursDispo'=> 'VAILABLE COURSES',
+'block1'=> 'INTENSIVE TRAINING',
+'block2'=> 'REGULAR TRAINING',
+'block3'=> 'ACCELERATED REGULAR TRAINING',
+'block4'=> 'PRIVATE LESSONS',
+'block5'=> 'HOLYDAY COURSES',
+'bac1'=> 'This formula is for people wishing to increase thier level in English in a shorter time.',
+'bac2'=> 'Regular training is divided into 6 levels and allows the learner to speak English fluently.',
+'bac3'=>'With this option the learner is able to speak English in a shorter time.',
+'bac4'=> 'MORE ABOUT',
+'bac5'=> 'You want to be trained at home or online and at your own pace, do not hesitate to contact us.',
+'bac6'=> 'Students!! During these holidays, accelerate your learning, and have more ease to study in English.',
+
+
+
+
+
+
+
+'niv'=> 'DIFFERENT LEVELS',
+
+'baniere'=> 'COMPLETE YOUR DREAM OF BILINGUALISM IN A FEW WEEKS. All our courses prepare candidates for international exams,
+           TOEFL, IELTS, TOEIC. Our courses are designed according to the Common European Framework of Reference for Languages',
+'discover'=> 'A basic ability to communicate and exchange information in a simple way.',
+'learn'=> 'An ability to deal with simple, straightforward information and begin to express oneself in familiar contexts.',
+'improve'=> 'The ability to express oneself in a limited way in familiar situations and to deal in a general way with
+         nonroutine information.',
+    'master'=> 'The capacity to achieve most goals and express oneself on a range of topics.',
+    'expert1'=> 'The ability to communicate with the emphasis on how well it is done, in terms of appropriacy, sensitivity and the
+        capacity to deal with unfamiliar topics.',
+    'expert2'=> 'The capacity to deal with material which is academic or cognitively demanding, and to use language to good
+         effect at a level of performance which may in certain respects be more advanced than that of an average native
+          speaker.',
+
+
+    'niveauxLANGUE'=> 'After your level test, we are able to admit you to one of the 6 language levels established by the Common
+               European Framework of Reference for Languages (CEFR).',
+
+
+];
---
+++ resources/views/anglais/anglais.blade.php
@@ -0,0 +1,169 @@
+@extends('layout')
+
+@section('content')
+    <div id="main_home">
+
+        <div class="message_and_image">
+            <div class="message_on_image">{% trans from 'anglais' %} baniere {% endtrans %}</div>
+
+            <div id="about_top" style="background-image:url('{{ asset('data/img/about4.jpg') }}')">
+                <img class="baniere" id="top_img" src="{{ asset('data/img/baniere1.jpg') }}" alt="">
+                {# il  est possible de travailler directement  ici. si  vous voulez occuper toute la page dans la width #}
+            </div>
+        </div>
+
+        <div class="flex-contain">
+            <div class="">
+
+
+                <div class="accueil">
+
+                    <div>
+                        {% trans from 'anglais' %} PlsFormats {% endtrans %}
+                        <ul>
+                            <li><a href="{{ ('main_CoursIntensifs') }}">{% trans from 'anglais' %} CoursIntens {% endtrans %}</a></li>
+                            <li><a href="{{ ('main_FormationContinue') }}">{% trans from 'anglais' %} FormatReg {% endtrans %}</a></li>
+                            <li><a href="{{ ('main_FormationContinue') }}">{% trans from 'anglais' %} FormatRegACC {% endtrans %}</a></li>
+                            <li><a href="{{ ('main_PourEntreprises') }}">{% trans from 'anglais' %} prEntrep {% endtrans %}</a> </li>
+                            <li><a href="{{ ('main_CoursDeVacances') }}">{% trans from 'anglais' %} CoursVACCANCES {% endtrans %}</a> </li>
+                            <li><a href="{{ ('main_CoursParticulier') }}">{% trans from 'anglais' %} CoursPARTICULIERS {% endtrans %}</a> </li>
+                        </ul>
+                    </div>
+
+                    <div>  {% trans from 'about' %} about.block15 {% endtrans %} </div>
+                    <div class="utiles">
+                        <a class="liens-utiles" href="{{ ('main_NiveauxDeLangue') }}">>>{% trans from 'anglais' %} diffNIVEAUX {% endtrans %}</a>
+                    </div>
+
+                </div>
+
+                <div class="row section admission">
+                    <h2 class="bloc-title"> {% trans from 'anglais' %} coursDispo {% endtrans %} </h2>
+                    <div class="">
+                        <div class="flex">
+                            <div class="hexagon degrade admission-hexagon">
+                                <div class="triangle-top"></div>
+                                <div class="titre"> {% trans from 'anglais' %} block1 {% endtrans %} </div>
+                                <h3>
+                                    <i class="icon-fleche-droite grand"></i>
+                                    <span class="adm-niv">IEPL</span>
+                                    <i class="icon-fleche-gauche grand"></i></h3>
+                                <div class="content">
+                                    {% trans from 'anglais' %} bac1 {% endtrans %}
+                                </div>
+                                <a class="myButton" href="{{ ('main_CoursIntensifs') }}">{% trans from 'anglais' %} bac4 {% endtrans %}</a>
+                                <div class="triangle-bottom"></div>
+                            </div>
+
+                            <div class="hexagon degrade admission-hexagon">
+                                <div class="triangle-top"></div>
+                                <div class="titre"> {% trans from 'anglais' %} block2 {% endtrans %} </div>
+                                <h3>
+                                    <i class="icon-fleche-droite grand"></i>
+                                    <span class="adm-niv">REPL</span>
+                                    <i class="icon-fleche-gauche grand"></i></h3>
+                                <div class="content">
+                                    {% trans from 'anglais' %} bac2 {% endtrans %}
+                                </div>
+                                <a class="myButton" href="{{ ('main_FormationContinue') }}">{% trans from 'anglais' %} bac4 {% endtrans %}</a>
+                                <div class="triangle-bottom"></div>
+                            </div>
+
+                            <div class="hexagon degrade admission-hexagon">
+                                <div class="triangle-top"></div>
+                                <div class="titre">{% trans from 'anglais' %} block3 {% endtrans %}</div>
+                                <h3>
+                                    <i class="icon-fleche-droite grand"></i>
+                                    <span class="adm-niv">REPL2</span>
+                                    <i class="icon-fleche-gauche grand"></i></h3>
+                                <div class="content">
+                                    {% trans from 'anglais' %} bac3 {% endtrans %}
+                                </div>
+                                <a class="myButton" href="{{ ('main_FormationContinue') }}">{% trans from 'anglais' %} bac4 {% endtrans %}</a>
+                                <div class="triangle-bottom"></div>
+                            </div>
+
+                            <div class="hexagon degrade admission-hexagon">
+                                <div class="triangle-top"></div>
+                                <div class="titre">{% trans from 'anglais' %} block4 {% endtrans %}</div>
+                                <h3>
+                                    <i class="icon-fleche-droite grand"></i>
+                                    <span class="adm-niv"></span>
+                                    <i class="icon-fleche-gauche grand"></i></h3>
+                                <div class="content">
+                                    {% trans from 'anglais' %} bac5 {% endtrans %}
+                                </div>
+                                <a class="myButton" href="{{ ('main_CoursParticulier') }}">{% trans from 'anglais' %} bac4 {% endtrans %}</a>
+                                <div class="triangle-bottom"></div>
+                            </div>
+
+                            <div class="hexagon degrade admission-hexagon">
+                                <div class="triangle-top"></div>
+                                <div class="titre">{% trans from 'anglais' %} block5 {% endtrans %}</div>
+                                <h3>
+                                    <i class="icon-fleche-droite grand"></i>
+                                    <span class="adm-niv"></span>
+                                    <i class="icon-fleche-gauche grand"></i></h3>
+                                <div class="content">
+                                    {% trans from 'anglais' %} bac6 {% endtrans %}
+                                </div>
+                                <a class="myButton" href="{{ ('main_CoursDeVacances') }}">{% trans from 'anglais' %} bac4 {% endtrans %}</a>
+                                <div class="triangle-bottom"></div>
+                            </div>
+
+
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+
+
+        <div class="separator"></div>
+
+
+        <div class="section programmes-main-div">
+            <h2 class="bloc-title">{% trans from 'about' %} about.news {% endtrans %}</h2>
+
+            <div class="inner padding">
+                <div class="flex">
+                    <div class="hexagon degrade admission-hexagon">
+                        <div class="news-title"><h4>{% trans from 'ctihome' %} english {% endtrans %}</h4></div>
+
+                        <p style="text-align: left">{% trans from 'ctihome' %} englishNews {% endtrans %}</p>
+                    </div>
+
+                    <div class="hexagon degrade admission-hexagon">
+                        <div class="news-title"><h4>{% trans from 'ctihome' %} german {% endtrans %}</h4></div>
+                        <p style="text-align: left">{% trans from 'ctihome' %} allemandNews {% endtrans %}</p>
+                    </div>
+
+                    <div class="hexagon degrade admission-hexagon">
+                        <div class="news-title"><h4>{% trans from 'ctihome' %} computing {% endtrans %}</h4></div>
+                        <p style="text-align: left">{% trans from 'ctihome' %} infoNews {% endtrans %}</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+@endsection
