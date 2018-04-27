109828c3bfea72d10c527cccaf815411ae8308f5
TAJORE <jouanirenatot@gmail.com>
2018-04-27 20:15:13 +0200

    sous page allemand pour cours particuliers

--- resources/lang/en/cours_particuliers_dallemand.php
+++ resources/lang/en/cours_particuliers_dallemand.php
@@ -1,10 +1,69 @@
+
 <?php
 
 return [
-    'title'=>'Private German courses.',
-    'title1'=>'specialise German courses.',
-    'descriptionPartAll'=> 'Private lessons of German for a progression at your pace of learning. CTI teachers are available to offer quality training. CTI is located in Bonamoussadi, Douala, Cameroon.',
-    'keywordPartAll'=> 'Individuals, Studies, Classes, Family Reunification, Germany, German, Courses, Becoming Bilingual, Training, Douala, Bonamoussadi, Cameroon, A1, A2, B1, B2, C1, C2, Goethe-zertifikat.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Traduction pour allemand en anglais
+    |--------------------------------------------------------------------------
+    |
+    | plus d'info...
+    |
+    */
+
+    'rentree'=> 'Next come back: April 30, 2018 ',
+    'text3'=> 'Registrations are in progress. There are still some places available. ',
+    'text4'=> 'PROGRAM ON MEASURE',
+    'text5'=> 'If you subcribe to private lessons, you benefit from a personalized training program: ',
+    'text6'=> 'suited to your sector of activity',
+    'text61'=> 'taking into account your language level and your real needs',
+    'text62'=> 'flexible schedule',
+    'text63'=> 'You can choose the course location',
+    'text64'=> 'We prepare candidates for the English tests Required to obtain a student visa, boost your career or your business (TOEFL, IELTS, TOEIC). ',
+
+    'text7'=> 'TEACHING MATERIALS',
+    'text8'=> 'The CTI is equipped with didactic tools suitable for easy and quick learning:',
+    'text81'=>'The best learning books',
+    'text82'=>'Audio, visual and video media for a better training',
+    'text83'=>'A well-stocked electronic library',
+    'text9'=> 'Goethe Zertifikat A1 (Start Deutsch 1): Menschen A1, 2 months + practical sessions',
+    'text10'=> 'Goethe Zertifikat A2 : Menschen A2, 2 months + practical sessions',
+    'text11'=> 'Goethe Zertifikat B1 : Aspekte B1, 2 months + practical sessions',
+    'text12'=> 'Goethe Zertifikat B2 : Aspekte B2, 2 months + practical sessions',
+    'text13'=> 'Goethe Zertifikat C1 : Aspekte C1, 2 months + practical sessions',
+    'text14'=> 'NB: Only levels A1, A2 and B1 are required to obtain a visa',
+    'text15'=> 'The quality of our training guarantees your success in the German language exams organized by the Goetthe Institute in Yaoundé; Goethe-zertifikat A1, A2, B1.',
+
+    'text16'=> 'COURSE SCHEDULES ',
+    'text17'=> 'If you subcribe to private lessons, you benefit from a personalized training program: ',
+    'text18'=> 'suited to your sector of activity',
+    'text19'=> 'taking into account your language level and your real needs',
+    'text20'=> 'flexible schedule',
+
+
+    'text21'=> '14H00 - 16h00',
+    'text22'=> 'Evening classes',
+    'text23'=> '18:30 - 20:30',
+    'text24'=> 'Private lessons',
+
+    'text241'=> 'PERIODS AND DIFFERENTS LEVELS ',
+    'text242'=> 'You evolve according to the six language levels of the CEFR (A1, A2, B1, B2, C1, C2). ',
+    'text243'=> 'Each level requires 5 weeks of training. ',
+    'text25'=> '5 days / week in intensive classes.',
+
+    'text261'=> 'Contact us if you wish to follow a training à la carte in one of our areas of expertise.',
+    'keywordEtudALL'=> 'Studies, Classes, Germany, German, Courses, Training, A1, A2, B1, B2, C1, C2, Goethe-zertifikat, Douala, Bonamoussadi, Cameroon',
+    'title1'=> 'PRIVATE COURSES VERY ACCESSIBLE !!',
+    'title2'=> 'GERMAN PRIVATE COURSES',
+    'descriptionALL'=> 'German lessons at your own pace for effective progression. Get your Goethe Zertifikat for your studies
+        in Germany thanks to the Churchil Training Institute located in Bonamoussadi, Douala, Cameroon.',
+    'keywordALL'=> 'German, Studies, Family Reunification, Germany, Course, Training, Goethe-zertifikat, Douala, Bonamoussadi, Cameroon, A1, A2, B1, B2, C1, C2',
+    'title'=> 'German courses at Douala with Churchill Training Institute - A reference language center.',
+    'title21'=> 'TRAINING  FOR GERMAN LANGUAGE EXAMS.',
+    'text1'=>'Would you like to take a private course to progress at your own pace and effectively? ',
+    'text2'=> 'The CTI provides its teachers for quality training.',
+
 
 
 ];
